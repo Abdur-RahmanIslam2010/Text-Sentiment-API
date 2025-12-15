7121f4c6c72d10bb7565c00ac1b3c68d0a3f3784
Abdur-RahmanIslam2010 <abdelrahmanislam116@gmail.com>
2025-12-15 17:43:35 +0200

    Project Initialized

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras import Sequential
