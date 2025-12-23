8688529b1cbd69a79c37fbf04882cd0c4b623f7d
Abdur-RahmanIslam2010 <abdelrahmanislam116@gmail.com>
2025-12-23 13:57:01 +0200

    Add API endpoints

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import load_model
 import pickle
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+from numpy import argmax
+import starlette.status as status
 
-df = pd.read_csv('dataset.csv', encoding='latin1')
-df = df.iloc[:, 1:]
-df.columns = ['sentiment', 'id', 'date', 'flag', 'user', 'text']
-X = df['text']
-y = df['sentiment'] / 2
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+app = FastAPI()
 
 with open('vectorizer.pkl', 'rb') as f:
     global vectorizer
     vectorizer = pickle.load(f)
 
-X_train_vec = vectorizer.transform(X_train)
-X_test_vec = vectorizer.transform(X_test)
-
-print(X_train_vec.shape)
 
 model = load_model('model.keras')
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.evaluate(X_test_vec, y_test)
+@app.get('/')
+async def root():
+    return RedirectResponse(url='/predict', status_code=status.HTTP_200_OK)
+
+class Text(BaseModel):
+    text: str
+
+@app.post('/predict')
+async def predict(prediction: Text):
+    text_to_predict = prediction.text
+    text_vec = vectorizer.transform([text_to_predict])
+    verdict = model.predict(text_vec)[0]
+    labels = ['negative', '','positive']
+    return {
+        "prediction": labels[argmax(verdict)],
+        "values": {
+            "positive": float(verdict[-1]),
+            "negative": float(verdict[0])
+        }
+    }
